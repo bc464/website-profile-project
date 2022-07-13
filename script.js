1311aee30ae350fc15123ce33fe812a0712768cf
brian <fil419@webmail.co.za>
2022-07-13 10:09:56 +0200

    changes and add modal on project

--- script.js
+++ script.js
@@ -69,3 +69,22 @@ function changeImg(){
 
 // Run function when page loads
 window.onload=changeImg;
+
+// MODAL
+let modal = document.getElementById("myModal");
+let btn = document.getElementById('myBtn');
+let span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function() {
+	modal.style.display = "block";
+}
+span.onclick = function() {
+	modal.style.display = "none";
+}
+window.onclick = function(event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+}
+
