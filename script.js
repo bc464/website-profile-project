2f1692669224ec1ad8a21983523ccadd0debc83f
brian <fil419@webmail.co.za>
2022-08-24 12:21:05 +0200

    updated frontend slider

--- script.js
+++ script.js
@@ -1,43 +1,39 @@
-let i = 0; 			
-let images = [];	
-let time = 5000;	
-	 
-// Image List
-images[0] = "./images/frontend/Chat-app-css-illustration.png";
-images[1] = "./images/frontend/E-commerce product page.png";
-images[2] = "./images/frontend/Easybank landing page.png";
-images[3] = "./images/frontend/Expenses chart component.png";
-images[4] = "./images/frontend/Advice generator app.png";
-images[5] = "./images/frontend/Bookmark landing page.png";
-images[6] = "./images/frontend/Calculator app.png";
-images[7] = "./images/frontend/Intro section with dropdown navigation.png";
-images[8] = "./images/frontend/IP Address Tracker.png";
-images[9] = "./images/frontend/Launch countdown timer.png";
-images[10] = "./images/frontend/Manage landing page.png";
-images[11] = "./images/frontend/NFT preview card component.png";
-images[12] = "./images/frontend/Order summary card.png";
-images[13] = "./images/frontend/QR code component.png";
-images[14] = "./images/frontend/Room homepage.png";
-images[15] = "./images/frontend/Shortly URL shortening API Challenge.png";
-images[16] = "./images/frontend/Sunnyside agency landing page.png";
-images[17] = "./images/frontend/Time tracking dashboard.png";
-images[18] = "./images/frontend/Tip calculator app.png";
-images[19] = "./images/frontend/Rock Paper Scissors.png";
+const slides = document.querySelectorAll(".slide");
+const nextBtn = document.querySelector(".nextBtn");
+const prevBtn = document.querySelector(".prevBtn");
 
-// Change Image
-function changeImg(){
-	document.slide.src = images[i];
-	
-	if(i < images.length - 1){
-	  i++; 
-	} else { 
-		i = 0;
+slides.forEach(function (slide, index) {
+	slide.style.left = `${index * 100}%`;
+});
+
+let counter = 0;
+nextBtn.addEventListener("click", function() {
+	counter++;
+	carousel();
+});
+prevBtn.addEventListener("click", function() {
+	counter--;
+	carousel();
+});
+
+function carousel() {
+	if (counter < slides.length -1) {
+		nextBtn.style.display = "block";
+	} else {
+		nextBtn.style.display = "none";
 	}
-	setTimeout("changeImg()", time);
+	if (counter > 0) {
+		prevBtn.style.display = "block";
+	} else {
+		prevBtn.style.display = "none";
+	}
+	slides.forEach(function (slide) {
+		slide.style.transform = `translateX(-${counter * 100}%`;
+	});
 }
 
-// Run function when page loads
-window.onload=changeImg;
+prevBtn.style.display = "none";
+
 
 // MODAL
 let modal = document.getElementById("myModal");
